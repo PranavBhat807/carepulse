32dbaac47e54ecd2741b87e3702ebce50de6244b
Pranav <134412219+PranavBhat807@users.noreply.github.com>
2024-08-15 13:07:55 +0530

    updated file

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,27 +1,84 @@
 "use server"
-import { ID, Query } from "node-appwrite"
-import { users } from "../appwrite.config"
-import { parseStringify } from "../utils"
+import { ID, Query } from "node-appwrite";
+import { BUCKET_ID, DATABASE_ID, databases, ENDPOINT, PATIENT_COLLECTION_ID, PROJECT_ID, storage, users } from "../appwrite.config";
+import { parseStringify } from "../utils";
+import { InputFile } from "node-appwrite/file";
 
 export const createUser = async (user: CreateUserParams) => {
     try {
-        const newUser =  await users.create(
+        const newUser = await users.create(
             ID.unique(), 
             user.email, 
             user.phone, 
             undefined, 
             user.name
-        )
-        console.log({newUser})
+        );
+        console.log({ newUser });
 
         return parseStringify(newUser);
-    } catch (error: any){
-        if(error && error?.code === 409) {
-        const documents = await users.list([
-            Query.equal('email',[user.email])
-        ])
+    } catch (error: any) {
+        if (error?.code === 409) {
+            try {
+                const documents = await users.list([
+                    Query.equal('email', [user.email])
+                ]);
 
-        return documents?.users[0]
+                if (documents?.users?.length > 0) {
+                    return parseStringify(documents.users[0]);
+                } else {
+                    throw new Error("User not found in the list response.");
+                }
+            } catch (listError) {
+                console.error("Error listing users:", listError);
+                throw listError;
+            }
+        } else {
+            console.error("Error creating user:", error);
+            throw error;
+        }
     }
-}
+};
+
+// GET USER
+export const getUser = async (userId: string) => {
+    try {
+      const user = await users.get(userId);
+  
+      return parseStringify(user);
+    } catch (error) {
+      console.error(
+        "An error occurred while retrieving the user details:",
+        error
+      );
+    }
+  };
+
+export const registerPatient = async({ identificationDocument, ...patient }: RegisterUserParams) => {
+    try {
+        let file;
+
+        if(identificationDocument) {
+            const inputFile = InputFile.fromBuffer(
+                identificationDocument?.get('blobFile') as Blob,
+                identificationDocument?.get('fileName') as string,
+            )
+
+            file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile)
+        }
+
+        const newPatient = await databases.createDocument(
+            DATABASE_ID!,
+            PATIENT_COLLECTION_ID!,
+            ID.unique(),
+            {
+                identificationDocumentId: file?.$id || null,
+                identificationDocumentUrl: `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file?.$id}/view?project=${PROJECT_ID}`,
+                ...patient
+            }
+        )
+        return parseStringify(newPatient)
+    } catch (error) {
+        console.log(error)
+        return null;
+    }
 }
